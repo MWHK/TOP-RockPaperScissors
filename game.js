efb06b35094b2c1c990a133a9a8aa66b36f6c4fa
MWHK <116496317+MWHK@users.noreply.github.com>
2023-02-27 15:10:37 +0800

    Commented code and epxlained functions clearly.

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
+//log of win and losses
 let winCounter = 0;
 let loseCounter = 0;
 
 function playRound() {
-  let items = ["rock", "paper", "scissors"];
-  let randomIndex = Math.floor(Math.random() * 3);
+  let items = ["rock", "paper", "scissors"]; //array of variables
+  let randomIndex = Math.floor(Math.random() * 3); 
   let randomItem = items[randomIndex];
-  let chosenItem = prompt("rock paper or scissors").toLowerCase();
+  let chosenItem = prompt("rock paper or scissors").toLowerCase(); //standarizing input to match array
   console.log("You Chose: " + chosenItem + " - Computer Chose: " + randomItem);
   if (chosenItem === randomItem) {
     console.log("It's a tie!");
@@ -13,11 +14,11 @@ function playRound() {
   } else if ((chosenItem === "rock" && randomItem === "scissors") || (chosenItem === "paper" && randomItem === "rock") || (chosenItem === "scissors" && randomItem === "paper")) {
     console.log("You win this round!");
     winCounter++;
-    console.log("Current Score: " + "player " + winCounter + " - Computer " + loseCounter);
+    console.log("Current Score: " + "player " + winCounter + " - Computer " + loseCounter); //displaying score
     if (winCounter < 5) {
       playRound()
     } else {
-      console.log("Game Over - YOU WIN!");
+      console.log("Game Over - YOU WIN!"); //end of game as a winner (more than 5 wins)
       return
     }
   } else {
@@ -27,7 +28,7 @@ function playRound() {
     if (loseCounter < 5) {
         playRound()
     } else {
-      console.log("Game Over - YOU LOSE!");
+      console.log("Game Over - YOU LOSE!"); //end of game as a winner (more than 5 loses)
       return
     }
   }
